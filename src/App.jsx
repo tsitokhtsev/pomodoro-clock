444395ce969bf40a8040132a8e47f80112ff4c36
Grigoli Tsitokhtsevi <tsitokhtsev2002@gmail.com>
2025-03-29 17:11:12 +0400

    refactor: convert App component to functional component

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Controller from './components/Controller';
 import Timer from './components/Timer';
 
-const beep = document.getElementById('beep');
+export default function App() {
+  const [sessionLength, setSessionLength] = useState(25);
+  const [breakLength, setBreakLength] = useState(5);
+  const [currentInterval, setCurrentInterval] = useState('session');
+  const [timerIsOn, setTimerIsOn] = useState(false);
+  const [time, setTime] = useState(25 * 60);
 
-class App extends React.Component {
-  constructor() {
-    super();
+  const intervalRef = useRef(null);
+  const beepRef = useRef(null);
 
-    this.state = {
-      sessionLength: 25,
-      breakLength: 5,
-      currentInterval: 'session',
-      timerIsOn: false,
-      time: 25 * 60,
-      interval: undefined,
+  useEffect(() => {
+    beepRef.current = document.getElementById('beep');
+
+    return () => {
+      clearInterval(intervalRef.current);
     };
-  }
+  }, []);
 
-  formatTime = (time) => {
+  const formatTime = (time) => {
     let minutes = Math.floor(time / 60);
     let seconds = time % 60;
     return `${minutes < 10 ? '0' + minutes : minutes}:${
@@ -26,130 +28,120 @@ class App extends React.Component {
     }`;
   };
 
-  handleLengthChange = (intervalType, changeType) => {
-    if (!this.state.timerIsOn) {
-      if (intervalType === this.state.currentInterval) {
-        if (
-          changeType === 'increment' &&
-          this.state[intervalType + 'Length'] < 60
-        ) {
-          this.setState({
-            [intervalType + 'Length']: this.state[intervalType + 'Length'] + 1,
-            time: (this.state[intervalType + 'Length'] + 1) * 60,
-          });
+  const handleLengthChange = (intervalType, changeType) => {
+    if (!timerIsOn) {
+      if (intervalType === currentInterval) {
+        if (changeType === 'increment' && eval(`${intervalType}Length`) < 60) {
+          if (intervalType === 'session') {
+            setSessionLength((prev) => prev + 1);
+            setTime((sessionLength + 1) * 60);
+          } else {
+            setBreakLength((prev) => prev + 1);
+            setTime((breakLength + 1) * 60);
+          }
         } else if (
           changeType === 'decrement' &&
-          this.state[intervalType + 'Length'] > 1
+          eval(`${intervalType}Length`) > 1
         ) {
-          this.setState({
-            [intervalType + 'Length']: this.state[intervalType + 'Length'] - 1,
-            time: (this.state[intervalType + 'Length'] - 1) * 60,
-          });
+          if (intervalType === 'session') {
+            setSessionLength((prev) => prev - 1);
+            setTime((sessionLength - 1) * 60);
+          } else {
+            setBreakLength((prev) => prev - 1);
+            setTime((breakLength - 1) * 60);
+          }
         }
       } else {
-        if (
-          changeType === 'increment' &&
-          this.state[intervalType + 'Length'] < 60
-        ) {
-          this.setState({
-            [intervalType + 'Length']: this.state[intervalType + 'Length'] + 1,
-          });
+        if (changeType === 'increment' && eval(`${intervalType}Length`) < 60) {
+          intervalType === 'session'
+            ? setSessionLength((prev) => prev + 1)
+            : setBreakLength((prev) => prev + 1);
         } else if (
           changeType === 'decrement' &&
-          this.state[intervalType + 'Length'] > 1
+          eval(`${intervalType}Length`) > 1
         ) {
-          this.setState({
-            [intervalType + 'Length']: this.state[intervalType + 'Length'] - 1,
-          });
+          intervalType === 'session'
+            ? setSessionLength((prev) => prev - 1)
+            : setBreakLength((prev) => prev - 1);
         }
       }
     }
   };
 
-  handlePlayPause = () => {
-    if (this.state.timerIsOn) {
-      clearInterval(this.state.interval);
-      this.setState({ timerIsOn: false });
+  const handlePlayPause = () => {
+    if (timerIsOn) {
+      clearInterval(intervalRef.current);
+      setTimerIsOn(false);
     } else {
-      this.setState({
-        interval: setInterval(() => {
-          if (this.state.time === 0) {
-            this.setState({
-              currentInterval:
-                this.state.currentInterval === 'session' ? 'break' : 'session',
-              time:
-                (this.state.currentInterval === 'session'
-                  ? this.state.breakLength
-                  : this.state.sessionLength) * 60,
-            });
+      intervalRef.current = setInterval(() => {
+        setTime((prevTime) => {
+          if (prevTime === 0) {
+            const nextInterval =
+              currentInterval === 'session' ? 'break' : 'session';
+            const nextTime =
+              (currentInterval === 'session' ? breakLength : sessionLength) *
+              60;
 
-            beep.play();
-          } else {
-            this.setState({ time: this.state.time - 1 });
+            setCurrentInterval(nextInterval);
+            beepRef.current.play();
+
+            return nextTime;
           }
-        }, 1000),
-        timerIsOn: true,
-      });
+
+          return prevTime - 1;
+        });
+      }, 1000);
+
+      setTimerIsOn(true);
     }
   };
 
-  handleReset = () => {
-    this.setState({
-      sessionLength: 25,
-      breakLength: 5,
-      currentInterval: 'session',
-      timerIsOn: false,
-      time: 25 * 60,
-      interval: undefined,
-    });
+  const handleReset = () => {
+    setSessionLength(25);
+    setBreakLength(5);
+    setCurrentInterval('session');
+    setTimerIsOn(false);
+    setTime(25 * 60);
 
-    clearInterval(this.state.interval);
-    beep.pause();
-    beep.currentTime = 0;
+    clearInterval(intervalRef.current);
+
+    if (beepRef.current) {
+      beepRef.current.pause();
+      beepRef.current.currentTime = 0;
+    }
   };
 
-  componentWillUnmount() {
-    clearInterval(this.state.interval);
-  }
+  return (
+    <>
+      <h1>Pomodoro Clock</h1>
 
-  render() {
-    return (
-      <React.Fragment>
-        <h1>Pomodoro Clock</h1>
+      <Timer currentInterval={currentInterval} time={formatTime(time)} />
 
-        <Timer
-          currentInterval={this.state.currentInterval}
-          time={this.formatTime(this.state.time)}
+      <div id="buttons">
+        <button id="start_stop" onClick={handlePlayPause}>
+          {timerIsOn ? 'Stop' : 'Play'}
+        </button>
+
+        <button id="reset" onClick={handleReset}>
+          Reset
+        </button>
+      </div>
+
+      <div id="controllers">
+        <Controller
+          title="Session Length"
+          time={sessionLength}
+          handleLengthChange={handleLengthChange}
+          intervalType="session"
         />
 
-        <div id="buttons">
-          <button id="start_stop" onClick={this.handlePlayPause}>
-            {this.state.timerIsOn ? 'Stop' : 'Play'}
-          </button>
-
-          <button id="reset" onClick={this.handleReset}>
-            Reset
-          </button>
-        </div>
-
-        <div id="controllers">
-          <Controller
-            title="Session Length"
-            time={this.state.sessionLength}
-            handleLengthChange={this.handleLengthChange}
-            intervalType="session"
-          />
-
-          <Controller
-            title="Break Length"
-            time={this.state.breakLength}
-            handleLengthChange={this.handleLengthChange}
-            intervalType="break"
-          />
-        </div>
-      </React.Fragment>
-    );
-  }
+        <Controller
+          title="Break Length"
+          time={breakLength}
+          handleLengthChange={handleLengthChange}
+          intervalType="break"
+        />
+      </div>
+    </>
+  );
 }
-
-export default App;
